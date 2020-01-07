ecf0bd7dc95a3485c992636e2179475152de3ceb
Adrian Martinez <adrianjustinmartinez@gmail.com>
2020-01-06 21:03:12 -0800

    Add feedback sound

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -4,6 +4,7 @@
 #include "FPSExtractionZone.h"
 #include "Components/BoxComponent.h"
 #include "Components/DecalComponent.h"
+#include "Kismet/GameplayStatics.h"
 #include "FPSCharacter.h"
 #include "FPSGameMode.h"
 
@@ -28,10 +29,14 @@ AFPSExtractionZone::AFPSExtractionZone()
 
 void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
-	UE_LOG(LogTemp, Log, TEXT("OVERLAPPED WITH EXTRACTION ZONE!"));
 
 	AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);
-	if (MyPawn && MyPawn->bIsCarryingObjective)
+	if (MyPawn == nullptr)
+	{
+		return;
+	}
+
+	if (MyPawn->bIsCarryingObjective)
 	{
 		AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
 		if (GM)
@@ -39,5 +44,11 @@ void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent,
 			GM->CompleteMission(MyPawn);
 		}
 	}
+	else
+	{
+		UGameplayStatics::PlaySound2D(this, ObjectiveMissingSound);
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("OVERLAPPED WITH EXTRACTION ZONE!"));
 }
 
